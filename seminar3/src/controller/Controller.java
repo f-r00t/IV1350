3873f675f77867b9350bb2b419b2023943aa0467
IsacAlmqvist <isacol@kth.se>
2025-04-29 14:44:03 +0200

    o

--- seminar3/src/controller/Controller.java
+++ seminar3/src/controller/Controller.java
@@ -1,5 +1,20 @@
 package controller;
 
-public class Controller {
+import integration.AccountingHandler;
+import integration.DiscountHandler;
+import integration.InventoryHandler;
+import integration.ReceiptPrinter;
 
+public class Controller {
+    InventoryHandler inventoryHandler;
+    AccountingHandler accountingHandler;
+    ReceiptPrinter receiptPrinter;
+    DiscountHandler discountHandler;
+
+    public Controller() {
+        inventoryHandler = new InventoryHandler();
+        accountingHandler = new AccountingHandler();
+        receiptPrinter = new ReceiptPrinter();
+        discountHandler = new DiscountHandler();
+    }
 }
---
+++ seminar3/test/modelTest/ReceiptTest.java
@@ -0,0 +1,5 @@
+package seminar3.test.modelTest;
+
+public class ModelTest {
+    
+}

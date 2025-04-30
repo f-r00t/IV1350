02b43f5b0ab28d4d62368aa50abdb8874403aa71
f-r00t <fruktstav@gmail.com>
2025-04-30 17:02:32 +0200

    Add unit tests

--- seminar3/test/integrationTest/DiscountHandlerTest.java
+++ seminar3/test/integrationTest/DiscountHandlerTest.java
@@ -1,5 +1,32 @@
 package integrationTest;
 
+import integration.DiscountHandler;
+import model.Sale;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DiscountHandlerTest {
-    
+
+    private DiscountHandler discountHandler;
+    private Sale dummySale;
+
+    @BeforeEach
+    public void setUp() {
+        discountHandler = new DiscountHandler();
+        dummySale = new Sale();
+    }
+
+    @Test
+    public void testGetDiscountKnownCustomer() {
+        float discount = discountHandler.getDiscount(1, dummySale);
+        assertEquals(0.15f, discount, 0.0001);
+    }
+
+    @Test
+    public void testGetDiscountMultipleKnownCustomers() {
+        assertEquals(0.1f, discountHandler.getDiscount(2, dummySale), 0.0001);
+        assertEquals(0.5f, discountHandler.getDiscount(3, dummySale), 0.0001);
+    }
 }
